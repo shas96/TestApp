583b1447cc93e611ea4e3760469c18250c5261c0
Shashidhar B R <shas.ec92@gmail.com>
2018-09-04 09:26:38 +0000

    test

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("MenuItem.controller.View1", {
+		onInit: function() {
+			this.byId("openMenu").attachBrowserEvent("tab keyup", function(oEvent) {
+				this._bKeyboard = oEvent.type === "keyup";
+			}, this);
+		},
+
+		handlePressOpenMenu: function(oEvent) {
+			var oButton = oEvent.getSource();
+			if (!this._menu) {
+				this._menu = new sap.ui.unified.Menu();
+				this._menu.addItem(new sap.ui.unified.MenuItem({text: "Hello", enabled: false}));
+				this._menu.addItem(new sap.ui.unified.MenuItem({text: "Hello1", enabled: true}));
+			}
+
+			var eDock = sap.ui.core.Popup.Dock;
+			this._menu.open(this._bKeyboard, oButton, eDock.BeginTop, eDock.BeginBottom, oButton);
+		}
+	});
+});
